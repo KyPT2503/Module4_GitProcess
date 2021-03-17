286ea8f7caf79e17ab70f10efcdfb3829cf04a66
Your Name <you@example.com>
2021-03-17 14:26:35 +0700

    create project

---
+++ src/main/java/com/example/git_process/GitProcessApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProcessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitProcessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git_process/GitProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
